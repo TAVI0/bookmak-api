4447abb95f5818c1a2696dd8ae9b5d56662eb899
TAVI0 <marcos_tavio@outlook.com>
2024-06-03 01:29:12 -0300

    avance sistema de autenticacion segun curso

--- src/main/java/com/tavio/bookmarkapi/web/controller/UserController.java
+++ src/main/java/com/tavio/bookmarkapi/web/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.tavio.bookmarkapi.web.controller;
 
 import com.tavio.bookmarkapi.domain.service.UserService;
-import com.tavio.bookmarkapi.persistance.entity.Post;
 import com.tavio.bookmarkapi.persistance.entity.User;
+import com.tavio.bookmarkapi.persistance.entity.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +14,9 @@ import java.util.Optional;
 public class UserController {
     @Autowired
     private UserService userService;
+ /*   @Autowired
+    private PasswordEncoder passwordEncoder;
+*/
     @GetMapping("/all")
     public List<User> getAll(){
         return userService.getAll();
@@ -26,8 +29,31 @@ public class UserController {
     public Optional<User> getByUsername(@PathVariable("username") String username){
         return userService.getByUsername(username);
     }
+
+    @GetMapping("/userexist/{username}")
+    public boolean getUserExistByUsername(@PathVariable("username") String username){
+        return userService.getByUsername(username).isPresent();
+    }
     @PostMapping("/save")
     public void save(@RequestBody User user){
+        //    String encryptedPassword = passwordEncoder.encode(user.getPassword());
+        //   user.setPassword(encryptedPassword);
         userService.save(user);
+    }
+
+    @PutMapping("/update")
+    public void update(@RequestBody User user){
+        //    String encryptedPassword = passwordEncoder.encode(user.getPassword());
+        //   user.setPassword(encryptedPassword);
+        userService.save(user);
+    }
+    @PostMapping("/login")
+    public Optional<UserResponse> login(@RequestBody User user){
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(1L);
+        userResponse.setUsername("TAVI0");
+        userResponse.setAccessToken("tokenDeAccesso");
+        userResponse.setRefreshToken("tokendeRefresco?(?");
+        return Optional.of(userResponse);
     }
 }
